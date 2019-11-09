c1c01782854cbaae8ae5640a5999e14bbb25c405
Toga <rodionphilippov@gmail.com>
2019-11-10 00:37:35 +0400

    done

---
+++ Vigenere/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Vigenere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cipher = new VigenereCipher(" АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ-.?!0123456789");
+
+            string path = @"D:\GERASKIN\Vigenere\text.txt";
+            var inputText = "";
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    inputText = sr.ReadToEnd().ToUpper();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Text: " + inputText);
+
+            Console.Write("Enter Key: ");
+            var password = Console.ReadLine().ToUpper();
+            var encryptedText = cipher.Encrypt(inputText, password);
+
+            Console.WriteLine("Result of encryption {0}", encryptedText);
+
+            Console.WriteLine("Result of decryption {0}", cipher.Decrypt(encryptedText, password));
+
+            Console.ReadLine();
+        }
+    }
+}
